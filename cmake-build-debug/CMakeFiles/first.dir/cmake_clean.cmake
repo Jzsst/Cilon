580986f437c39d3f6bb62404f13debc93f437a1a
Jzsst <2863362784@qq.com>
2018-11-06 23:06:25 +0800

    首次提交

---
+++ cmake-build-debug/CMakeFiles/first.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.dir/main.c.obj"
+  "CMakeFiles/first.dir/test.c.obj"
+  "first.pdb"
+  "first.exe"
+  "first.exe.manifest"
+  "libfirst.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
